0e6dbdd6a9a72bc5fd17c45484558359dca1fac4
Daniel <Daniel@Daniel-pc>
2018-05-02 11:40:37 -0300

    primeiro commit

---
+++ teste-maven-01/src/main/java/ClasseDeTeste.java
@@ -0,0 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
+
+public class ClasseDeTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(StringUtils.capitalize("Daniel"));
+
+	}
+
+}
---
+++ teste-maven-01/src/test/java/SuperUtilTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SuperUtilTest {
+
+    @Test
+    public void escapeHTMLTest() {
+
+        String escapedHtml = SuperUtil.escapeHTML("<script>alert(1);</script>");
+        String expected = "&lt;script&gt;alert(1);&lt;/script&gt;"; 
+        Assert.assertEquals(expected, escapedHtml);
+
+    }
+
+}
